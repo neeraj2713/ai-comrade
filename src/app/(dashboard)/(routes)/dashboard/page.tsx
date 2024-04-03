2c791be79e07ae8ea37c68b07b30e127f92fcb67
Neeraj Shekhawat <neeraj.shekhawat2003@gmail.com>
2024-04-04 01:00:35 +0530

    authorization setup

--- src/app/(dashboard)/(routes)/dashboard/page.tsx
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,9 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
 
 
 const DashboardPage = () => {
   return (
     <div>
       <h1>Dashboard (Protected)</h1>
+      <UserButton afterSignOutUrl="/"/>
     </div>
   );
 }
