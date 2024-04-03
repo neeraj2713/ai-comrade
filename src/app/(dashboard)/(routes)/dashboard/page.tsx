ddef71ea0cd6447c5b7ef22229f2f84fec8df164
Neeraj Shekhawat <neeraj.shekhawat2003@gmail.com>
2024-04-04 00:26:04 +0530

    folder setup

---
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1>Dashboard (Protected)</h1>
+    </div>
+  );
+}
+
+export default DashboardPage;
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
-  return (
-    <div>
-      <p>hello ai comrade</p>
-    </div>
-  );
-}
